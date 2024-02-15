722e2cfec35be1631ad8baf1f8db2b11aaeabc4e
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-15 20:56:19 +0800

    Caching get file contents

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,15 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('post/{post}', function ($post) {
-    $filePath = __DIR__ . "/../resources/post/{$post}.html";
-
-    if(!file_exists($filePath)){
+Route::get('post/{slug}', function ($slug) {
+    
+    if(!file_exists($filePath =  __DIR__ . "/../resources/post/{$slug}.html")){
         abort(404);
     }
+
+    // cache the filepath content for 20 seconds
+    $post = cache()->remember("post.{$slug}", now()->addSeconds(30) ,fn() => file_get_contents($filePath));
     
-    $post = file_get_contents($filePath);
 
     return view('post',[
         'post' => $post
