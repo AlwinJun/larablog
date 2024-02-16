6d3265d63e20d0e7d85305cac05bba6050e5bf36
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-16 21:21:26 +0800

    Add all functions to  get all the post/file.html content

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,16 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
-class Post extends Model
+class Post
 {
     use HasFactory;
+
+    public static function all()
+    {
+        $files = File::files(resource_path("post/"));
+
+        return array_map(fn($file) => $file->getContents(), $files);
+    }
 
     public static function find($slug)
     {
         if (!file_exists($filePath = resource_path("post/{$slug}.html"))) {
-            abort(404);
+            throw new ModelNotFoundException();
         }
 
         // cache the filepath content for 20 seconds
