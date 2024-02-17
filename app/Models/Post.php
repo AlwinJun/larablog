3bcecc9b493593136573a21fcf0e150ce49d84c0
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-17 22:19:48 +0800

    Update Post model. add contructor and get file contents as an object

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,20 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+// use Illuminate\Database\Eloquent\Model;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post
 {
     use HasFactory;
 
+    public function __construct(public $title, public $exerpt, public $date, public $slug, public $body)
+    {
+        $this->title = $title;
+        $this->exerpt = $exerpt;
+        $this->date = $date;
+        $this->slug = $slug;
+        $this->body = $body;
+    }
+
     public static function all()
     {
         $files = File::files(resource_path("post/"));
 
-        return array_map(fn($file) => $file->getContents(), $files);
+        $post = array_map(function ($file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            return new Post(
+                $document->title,
+                $document->exerpt,
+                $document->date,
+                $document->slug,
+                $document->body()
+            );
+
+        }, $files);
+
+        return $post;
+
+        // return array_map(fn($file) => $file->getContents(), $files);
     }
 
     public static function find($slug)
