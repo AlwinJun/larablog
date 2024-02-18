f9167773a73ec05c6d5f10ca570f4b159b5faa2e
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-18 16:29:01 +0800

    Render views with blade components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <link rel="stylesheet" href="/index.css">
-
-    <title>Document</title>
-</head>
-
-<body>
+<x-layout>
     <main>
         <article>
             <h2>
@@ -21,6 +8,4 @@
         </article>
         <a href="/">Go back</a>
     </main>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <link rel="stylesheet" href="/index.css">
-
-    <title>Document</title>
-</head>
-
-<body>
+<x-layout>
     <main>
         @foreach ($posts as $post)
             <article>
@@ -24,6 +11,4 @@
             </article>
         @endforeach
     </main>
-</body>
-
-</html>
+</x-layout>
