2992320c58b6aa5ce6cdaa274ebea4444d079dd9
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-21 21:57:16 +0800

    Generate random user and temp add hardcoded records to be insert for post and category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +16,73 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $food = Category::create([
+            'name' => 'Food',
+            'slug' => 'food'
+        ]);
+
+        $laravel = Category::create([
+            'name' => 'Laravel',
+            'slug' => 'laravel'
+        ]);
+
+        $hobby = Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby'
+        ]);
+
+        $job = Category::create([
+            'name' => 'Job',
+            'slug' => 'Job'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $food->id,
+            'title' => 'My Food Post',
+            'slug' => 'my-food-post',
+            'exerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $laravel->id,
+            'title' => 'Laravel Updates',
+            'slug' => 'laravel-updates',
+            'exerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobby->id,
+            'title' => 'Interesting Hobbies',
+            'slug' => 'interesting-hobbies',
+            'exerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $job->id,
+            'title' => 'Job Post',
+            'slug' => 'job-post',
+            'exerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
