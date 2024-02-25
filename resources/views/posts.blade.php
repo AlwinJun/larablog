e2adff0b298e1df73287b5f29122582956fa4b7b
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-25 14:09:43 +0800

    Render post data from the db

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,37 +1,20 @@
 <x-layout>
     @include('/partials/_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-post-featured-card :post='$posts[0]' />
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-6">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post='$post'
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No post yet,come back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
-
-
-    {{-- <main>
-        @foreach ($posts as $post)
-            <article>
-                <h2>
-                    <a href="/post/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h2>
-                <span>
-                    <span>
-                        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
-                    </span>
-                    <a href="/categories/{{ $post->category->slug }}" class="category">#{{ $post->category->name }}</a>
-                </span>
-                <p>{{ $post->exerpt }}</p>
-            </article>
-        @endforeach
-    </main> --}}
 </x-layout>
