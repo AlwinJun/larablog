3fd523b8914ecd09f321939e84b308d94494528c
AlwinJun <mahidlawonalwin@gmail.com>
2024-02-28 22:00:44 +0800

    Extract dropdown to its own component

--- resources/views/partials/_posts-header.blade.php
+++ resources/views/partials/_posts-header.blade.php
@@ -13,42 +13,32 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
-            <div x-data="{ show: false }">
-                <button type="button" @click="show = !show"
-                    class="flex lg:inline-flex w-full lg:w-36 py-2 pl-3 text-left text-sm font-semibold">
-                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Category' }}
+            <x-dropdown-category>
+                <x-slot:trigger>
+                    <button type="button"
+                        class="flex lg:inline-flex w-full lg:w-36 py-2 pl-3 text-left text-sm font-semibold">
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Category' }}
 
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d=" M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                            </path>
-                        </g>
-                    </svg>
-                </button>
-                <div x-show="show" class="absolute z-10 w-full py-2 bg-gray-100 rounded-xl mt-2" style="display: none">
-                    <a href="/"
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focus:text-white">All
-                    </a>
-                    @foreach ($categories as $category)
-                        <a href="/categories/{{ $category->slug }}"
-                            class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focus:text-white
-                            {{ isset($currentCategory) && $currentCategory->slug === $category->slug ? 'bg-blue-500 text-white' : '' }}">
-                            {{ ucwords($category->name) }}</a>
-                    @endforeach
-                </div>
-            </div>
+                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;"
+                            width="22" height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d=" M21 1v20.16H.84V1z">
+                                </path>
+                                <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                                </path>
+                            </g>
+                        </svg>
+                    </button>
+                </x-slot:trigger>
 
-            {{-- <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category
-                </option>
-                <option value="personal">Personal</option>
-                <option value="business">Business</option>
-            </select> --}}
-
+                {{-- default slot: items --}}
+                <x-dropdown-item href="/">All</x-dropdown-item>
+                @foreach ($categories as $category)
+                    <x-dropdown-item href="/categories/{{ $category->slug }}" :active="isset($currentCategory) && $currentCategory->is($category)">
+                        {{ ucwords($category->name) }}</x-dropdown-item>
+                @endforeach
+            </x-dropdown-category>
         </div>
 
         <!-- Other Filters -->
