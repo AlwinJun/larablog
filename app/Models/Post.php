e244d169dad57feabd0ccd8c5aed0c3d97a9ff3d
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-01 22:45:57 +0800

    Add local query scope to filter searches

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
@@ -13,6 +15,18 @@ class Post extends Model
 
     protected $guarded = [];
     protected $with = ['category', 'author'];
+
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn($query, $search) =>
+            $query->where('title', 'like', "%{$search}%")
+                ->orWhere('exerpt', 'like', "%{$search}%")
+        );
+    }
+
 
     public function category()
     {
