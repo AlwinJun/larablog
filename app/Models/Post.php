7e25efc487d33f6a5631012383185d04bf61598c
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-03 22:19:15 +0800

    Add filter for categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,6 +25,18 @@ class Post extends Model
             $query->where('title', 'like', "%{$search}%")
                 ->orWhere('exerpt', 'like', "%{$search}%")
         );
+
+        $query->when(
+            $filters['category'] ?? false,
+            fn($query, $category) =>
+            $query->whereHas(
+                'category',
+                fn($query) =>
+                $query->where('slug', $category)
+            )
+        );
+
+
     }
 
 
