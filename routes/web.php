2b895865641594457fe2a4d0cc3aef64576bf9a0
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-04 15:23:39 +0800

    Remove routes fo post author

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('post/{post:slug}', [PostController::class, 'show'])->name('post');
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all(),
-    ]);
-})->name('author');
