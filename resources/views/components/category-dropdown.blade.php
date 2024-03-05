806e5cabaff3e465118c92747acc5689ab22251c
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-05 22:39:10 +0800

    Add page as an exeption to query string when selecting category

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -9,11 +9,11 @@
         </x-slot>
 
         {{-- default slot: items --}}
-        <x-dropdown-item href="/?{{ http_build_query(request()->except('category')) }}"
+        <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}"
             :active="!isset($currentCategory)">All</x-dropdown-item>
         @foreach ($categories as $category)
             <x-dropdown-item
-                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
                 :active="$category->is($currentCategory)">
                 {{ ucwords($category->name) }}</x-dropdown-item>
         @endforeach
