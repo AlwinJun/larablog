cd3fb0d1954aa9a0514ace2e864fdcba5c4a734a
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-17 14:23:01 +0800

    Add comment component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,12 @@
+<article class="flex gap-4 rounded-xl border border-gray-200 bg-gray-100 p-5">
+    <div class="shrink-0">
+        <img src="https://i.pravatar.cc/70" alt="profile picture" class="rounded-xl">
+    </div>
+    <div>
+        <h3 class="font-bold">Alwin Jun</h3>
+        <p class="text-xs">Posted <time>2 days ago</time></p>
+        <p class="mt-2 leading-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, mollitia
+            voluptatum! Esse quibusdam consequatur doloremque dolorum aliquam molestiae cumque?
+            Blanditiis vitae, harum laboriosam impedit dolorem adipisci omnis enim fugit quaerat.</p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,7 +49,11 @@
                     {!! $post->body !!}
                 </div>
             </div>
+            <section class="col-span-8 col-start-5 mt-8 space-y-4">
+                <x-post-comment />
+            </section>
         </article>
+
     </main>
 
 
