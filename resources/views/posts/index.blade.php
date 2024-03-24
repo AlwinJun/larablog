0e0d14bc25d15c222cbf1c778837202f590a14a5
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-24 22:33:45 +0800

    Update views/posts folder

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('partials._posts-header')
+    @include('posts._posts-header')
     <main class="mx-auto mt-6 max-w-6xl space-y-6 lg:mt-20">
         @if ($posts->count())
             <x-post-featured-card :post='$posts[0]' />
