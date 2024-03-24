30a2d347a5bf3f933a2794f517a18badedb3e3f6
AlwinJun <mahidlawonalwin@gmail.com>
2024-03-24 22:23:40 +0800

    Minor changes

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,6 +1,6 @@
 @props(['comment'])
 
-<article class="flex gap-4 rounded-xl border border-gray-200 bg-gray-100 p-5">
+<article class="panel-border flex gap-4 bg-gray-100">
     <div class="shrink-0">
         <img src="https://i.pravatar.cc/70?u={{ $comment->author->id }}" alt="profile picture" class="rounded-xl">
     </div>
